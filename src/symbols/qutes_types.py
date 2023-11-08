10694939ce3471980aeb11665df7f4c2f3901b51
Gabriele Messina <gabryelemessina99@gmail.com>
2023-11-08 22:48:18 +0000

    Refactor qubit literal in grammar and implementation

--- src/symbols/qutes_types.py
+++ src/symbols/qutes_types.py
@@ -10,8 +10,8 @@ class Spin(Enum):
 
 class Qubit():
     def fromstr(self, literal : str) -> 'Qubit':
+        literal = literal.removesuffix(QutesParser.literaltostring(QutesParser.QUBIT_LITERAL_POSTFIX))
         if(literal.startswith(QutesParser.literaltostring(QutesParser.SQUARE_PARENTHESIS_OPEN))):
-            literal = literal.removesuffix(QutesParser.literaltostring(QutesParser.QUBIT_LITERAL_POSTFIX))
             literal = literal.removesuffix(QutesParser.literaltostring(QutesParser.SQUARE_PARENTHESIS_CLOSE))
             literal = literal.removeprefix(QutesParser.literaltostring(QutesParser.SQUARE_PARENTHESIS_OPEN))
             values = literal.split(',', 1)
@@ -23,27 +23,28 @@ class Qubit():
                     self.__init__(complex(1), complex(0))
                 else:
                     self.__init__(complex(0), complex(1))
-        elif(len(literal) == 2):
+        elif(len(literal) == 1):
             if(literal.startswith('0')): self.__init__(complex(1), complex(0))
             elif(literal.startswith('1')): self.__init__(complex(0), complex(1))
-            elif(literal.startswith('+')): self.__init__(complex(0.5), complex(0.5))
-            elif(literal.startswith('-')): self.__init__(complex(0.5), complex(-0.5))
         elif(len(literal) == 3):
-            if(literal.startswith('+0')): self.__init__(complex(1), complex(0))
-            elif(literal.startswith('+1')): self.__init__(complex(0), complex(1))
-            elif(literal.startswith('-0')): self.__init__(complex(-1), complex(0))
-            elif(literal.startswith('-1')): self.__init__(complex(0), complex(-1))
+            if(literal.startswith('|0>')): self.__init__(complex(1), complex(0))
+            elif(literal.startswith('|1>')): self.__init__(complex(0), complex(1))
+            elif(literal.startswith('|+>')): self.__init__(complex(0.5), complex(0.5))
+            elif(literal.startswith('|->')): self.__init__(complex(0.5), complex(-0.5))
         else:
-            literal = literal.removesuffix(QutesParser.literaltostring(QutesParser.QUBIT_LITERAL_POSTFIX))
             (alpha, beta) = literal.split(',', 1)
             self.__init__(complex(alpha), complex(beta))
         return self
+        
+    #TODO: implement type casting
+    # def __bool__(self, obj):
+    #     print("=============", obj)
     
     def __init__(self, alpha : complex = complex(), beta : complex = complex()):
         self.alpha = alpha
         self.beta = beta
         # Qubit has negative phase if alpha and beta have different sign?
-        self.phase = Spin.Positive if (alpha * beta).real < 0 else Spin.Negative 
+        self.phase = Spin.Positive if (alpha * beta).real >= 0 else Spin.Negative 
         self.is_superposition = cmath.isclose(abs(alpha), abs(beta))
 
     def __to_printable__(self) -> str:
@@ -64,45 +65,12 @@ class Quint():
     def fromstr(self, literal : str) -> 'Qubit':
         self.__init__(literal)
         return self
-        if(literal.startswith(QutesParser.literaltostring(QutesParser.SQUARE_PARENTHESIS_OPEN))):
-            literal = literal.removesuffix(QutesParser.literaltostring(QutesParser.QUBIT_LITERAL_POSTFIX))
-            literal = literal.removesuffix(QutesParser.literaltostring(QutesParser.SQUARE_PARENTHESIS_CLOSE))
-            literal = literal.removeprefix(QutesParser.literaltostring(QutesParser.SQUARE_PARENTHESIS_OPEN))
-            values = literal.split(',', 1)
-            n_values = len(values)
-            if(n_values == 2):
-                self.__init__(complex(1/n_values), complex(1/n_values))
-            else:
-                if(values[0] == '0'):
-                    self.__init__(complex(1), complex(0))
-                else:
-                    self.__init__(complex(0), complex(1))
-        elif(len(literal) == 2):
-            if(literal.startswith('0')): self.__init__(complex(1), complex(0))
-            elif(literal.startswith('1')): self.__init__(complex(0), complex(1))
-            elif(literal.startswith('+')): self.__init__(complex(0.5), complex(0.5))
-            elif(literal.startswith('-')): self.__init__(complex(0.5), complex(-0.5))
-        elif(len(literal) == 3):
-            if(literal.startswith('+0')): self.__init__(complex(1), complex(0))
-            elif(literal.startswith('+1')): self.__init__(complex(0), complex(1))
-            elif(literal.startswith('-0')): self.__init__(complex(-1), complex(0))
-            elif(literal.startswith('-1')): self.__init__(complex(0), complex(-1))
-        else:
-            literal = literal.removesuffix(QutesParser.literaltostring(QutesParser.QUBIT_LITERAL_POSTFIX))
-            (alpha, beta) = literal.split(',', 1)
-            self.__init__(complex(alpha), complex(beta))
-        return self
     
     def __init__(self, value:str = ""):
         self.value = value
 
     def __to_printable__(self) -> str:
         return f"{self.value}"
-        spin_str = '+' if self.phase == Spin.Positive else '-'
-        if(self.is_superposition):
-            return f"|{spin_str}>"
-        else:
-            return f"[(\u03B1:{self.alpha})|0> {spin_str} (\u03B2:{self.beta})|1>]"
 
     def __str__(self) -> str:
         return self.__to_printable__()
