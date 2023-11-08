97f9529636b14c61175ce47d4e5fcae10abcea79
Gabriele Messina <gabryelemessina99@gmail.com>
2023-11-08 23:36:52 +0000

    Add missing full adder gate to qutes_gates

--- src/qutes_gates.py
+++ src/qutes_gates.py
@@ -10,6 +10,38 @@ class Quint():
     def sum(self, other):
         return quantum_sum(self.size, self, other)
 
+def full_adder_gate():
+    # Creating a circuit with 8 quantum bits and 2 classical bits
+    qc = QuantumCircuit(8)
+
+    # Preparing inputs
+    qc.x(0) # Comment this line to make Qbit0 = |0>
+    qc.x(1) # Comment this line to make Qbit1 = |0>
+    qc.x(2) # Comment this line to make Qbit2 = |0> ( carry-in bit )
+
+    # AND gate1 implementation
+    qc.ccx(0,1,3)
+
+    # OR gate1 implementation
+    qc.cx(0,4) 
+    qc.cx(1,4)
+
+    # OR gate2 implementation
+    qc.cx(2,5) 
+    qc.cx(4,5)
+
+    # AND gate2 implementation
+    qc.ccx(2,4,6)
+
+    # OR gate implementation
+    qc.x(3)
+    qc.x(6)
+    qc.ccx(3,6,7)
+    qc.x(7)
+
+    gate_add = qc.to_gate()
+    gate_add.name = "full_adder"
+    return gate_add
 
 def quantum_sum(number_bits_number, a, b):
     a_qbits = QuantumRegister(number_bits_number, "a_qbits")
