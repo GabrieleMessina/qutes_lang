61df2620e587a20909a3d14ab0d7d029db222fcc
Gabriele Messina <gabryelemessina99@gmail.com>
2023-11-09 01:11:13 +0000

    Handle quantum variable initialization in circuit WIP

--- src/symbols/quantum_circuit_handler.py
+++ src/symbols/quantum_circuit_handler.py
@@ -19,24 +19,27 @@ class QuantumCircuitHandler():
 
     def __init__(self):
         self.quantum_registers : list[QuantumRegister] = []
+        self.quantum_registers_values : dict[QuantumRegister, list[float] | None] = {}
         self.classic_registers : list[ClassicalRegister] = [] 
         self.operation_stack : list[(QuantumGates, list[QuantumRegister])] = [] 
         self.varname_to_register : dict[str, QuantumRegister] = {} 
 
     def declare_variable(self,  variable_name : str, quantum_variable : any) -> QuantumRegister:
         new_register = None
+        initial_state = None,
         if(isinstance(quantum_variable, Qubit)):
             new_register = QuantumRegister(1, variable_name)
+            initial_state = [quantum_variable.alpha, quantum_variable.beta]
         if(isinstance(quantum_variable, Quint)):
             new_register = QuantumRegister(quantum_variable.size, variable_name)
+            initial_state = None #TODO: What goes here?
 
         if(new_register is None):
             raise SystemError("Error trying to declare a quantum variable of unsupported type")
 
-        #TODO: init with actual value
-
         self.varname_to_register[variable_name] = new_register
         self.quantum_registers.append(new_register)
+        self.quantum_registers_values[new_register] = initial_state
         return new_register
     
     def udpate_variable(self,  variable_name : str, quantum_variable : any) -> None:
@@ -44,18 +47,21 @@ class QuantumCircuitHandler():
         if(register_to_update is None):
             raise SystemError("Error trying to update an undeclared quantum variable")
 
-        #TODO: update with actual value
         if(isinstance(quantum_variable, Qubit)):
-            pass
-            #register_to_update = QuantumRegister(1, variable_name)
+            updated_state = [quantum_variable.alpha, quantum_variable.beta]
         if(isinstance(quantum_variable, Quint)):
+            #TODO: handle quint update
             pass
-            #register_to_update = QuantumRegister(quantum_variable.size, variable_name)
 
-        return register_to_update
+        self.quantum_registers_values[register_to_update] = updated_state
     
     def print_circuit(self):
         circuit = QuantumCircuit(*self.quantum_registers, *self.classic_registers)
+        for register in self.quantum_registers:
+            circuit.initialize(self.quantum_registers_values[register], register, True)
+        
+        circuit.barrier()
+
         for (operation, registers) in self.operation_stack:
             if(operation == QuantumGates.X):
                 circuit.x(*registers)
@@ -65,6 +71,9 @@ class QuantumCircuitHandler():
                 circuit.z(*registers)
             if(operation == QuantumGates.H):
                 circuit.h(*registers)
+        
+        circuit.barrier()
+        
         print(circuit.draw())
 
     def push_not_operation(self, QuantumRegister) -> None:
