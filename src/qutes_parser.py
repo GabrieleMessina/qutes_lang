f6f1d83f1747156305a719d3f2b05ca71151f7ea
Gabriele Messina <gabryelemessina99@gmail.com>
2023-12-23 23:42:04 +0000

    fix error mismatched input 'q' expecting SYMBOL_LITERAL

--- src/qutes_parser.py
+++ src/qutes_parser.py
@@ -1,5 +1,9 @@
 from qutes_antlr.qutes_parser import qutes_parser
 
 class QutesParser(qutes_parser):
+    QUBIT_LITERAL_POSTFIX = "'q'"
+    __custom_literal_names__ = [QUBIT_LITERAL_POSTFIX]
     def literal_to_string(literal) -> str:
-        return qutes_parser.literalNames[literal].removeprefix("'").removesuffix("'")
+        if(literal in QutesParser.__custom_literal_names__): 
+            return literal.removeprefix("'").removesuffix("'")
+        else: return qutes_parser.literalNames[literal].removeprefix("'").removesuffix("'")
