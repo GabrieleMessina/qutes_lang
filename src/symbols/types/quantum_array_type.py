eb520cf5a69dd2993434cae9946c6383332d2d3f
Gabriele Messina <gabryelemessina99@gmail.com>
2024-08-18 21:26:56 +0000

    Add print method to QuantumArrayType

--- src/symbols/types/quantum_array_type.py
+++ src/symbols/types/quantum_array_type.py
@@ -7,3 +7,6 @@ class QuantumArrayType(QuantumType[T]):
     def __init__(self, unit_type: Type[T]):
         super().__init__(unit_type)
         self.unit_type = unit_type
+
+    def __to_printable__(self) -> str:
+        return f"{self.unit_type.__name__}[]"
