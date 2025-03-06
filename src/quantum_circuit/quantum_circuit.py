260d2d264d636c16b3e36b60ac701300f785f41e
Gabriele Messina <g.messina@neoteksolutions.it>
2025-03-06 19:15:46 +0100

    Add better debug representation to quantum_circuit.py

--- src/quantum_circuit/quantum_circuit.py
+++ src/quantum_circuit/quantum_circuit.py
@@ -1,4 +1,7 @@
 from qiskit import QuantumCircuit as qc
 
 class QuantumCircuit(qc):
-    pass
+    def __repr__(self):
+        """Return the official string representing the register."""
+        self._repr = f"{self.__class__.__qualname__} {self.name}(qubits: {self.num_qubits}, clbits: {self.num_clbits})"
+        return self._repr
